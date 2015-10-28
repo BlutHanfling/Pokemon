62664b3787695a152cf6d324bb556faaea016919
BlutHanfling <squirkiz@gmail.com>
2015-10-28 14:19:27 +0100

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+	
+	public static void main (String args[]) {
+		int[] pika={10,5,4,6,4,7};
+		Pokemon pikachu=new Pokemon("Pikachu","Electrique",pika, "Statik");
+		System.out.println(pikachu.toString());
+			
+		
+	
+	}
+}
